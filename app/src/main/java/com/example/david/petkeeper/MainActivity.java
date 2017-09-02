cf5792c1b4532aefd19488470bdcdc67650a0115
David Murphy <djmgeneseo@gmail.com>
2017-09-02 03:13:16 -0400

    Nav bar doesn't work, needs debugging

--- app/src/main/java/com/example/david/petkeeper/MainActivity.java
+++ app/src/main/java/com/example/david/petkeeper/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.david.petkeeper;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    ActionBar actionBar;
     private ImageView dogView;
     private ImageView catView;
 
@@ -17,10 +27,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Nav Bar
+        actionBar = getSupportActionBar();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayoutId);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                actionBar.setTitle(getResources().getString(R.string.app_name));
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                actionBar.setTitle(getResources().getString(R.string.app_name));
+            }
+        };
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
+        LinearLayout row1 = (LinearLayout) findViewById(R.id.row1);
+
+        row1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDrawerLayout.closeDrawers();
+                Toast.makeText(MainActivity.this, "Pet House Selected", Toast.LENGTH_SHORT).show();
+            }
+        }); // row1 onClick()
+
+        // Dog or Cat Select Screen
         dogView = (ImageView) findViewById(R.id.dogId);
         catView = (ImageView) findViewById(R.id.catId);
 
-        dogView.setOnClickListener(this); // this means it's registered to our custom
+        dogView.setOnClickListener(this); // this means onClick is registered to our custom
                                           // onClickListener, described in this activity
         catView.setOnClickListener(this);
 
