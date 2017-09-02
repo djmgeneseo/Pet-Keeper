e27342db309eb0d0d2f9ae14fb5814d1c49bee0b
David Murphy <djmgeneseo@gmail.com>
2017-09-02 00:28:26 -0400

    Save function

--- app/src/main/java/com/example/david/petkeeper/KeeperActivity.java
+++ app/src/main/java/com/example/david/petkeeper/KeeperActivity.java
@@ -1,11 +1,18 @@
 package com.example.david.petkeeper;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class KeeperActivity extends AppCompatActivity {
 
@@ -36,6 +43,29 @@ public class KeeperActivity extends AppCompatActivity {
         extras = getIntent().getExtras();
         setUp(extras.getString("animal"));
 
+        // Save Pet
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Save pet info
+                SharedPreferences sharedPref = getSharedPreferences("pet", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("name", petNameEdit.getText().toString());
+                editor.putString("nickname", nickNameEdit.getText().toString());
+                editor.putString("breed", breedEdit.getText().toString());
+                editor.putString("bio", bioEdit.getText().toString());
+                editor.apply();
+
+                //DEBUGGING
+                //Log.d("Pet Name:", sharedPref.getString("name", ""));
+
+                // Toast alerts user that data is saved
+                Toast.makeText(KeeperActivity.this, "Pet was saved successfully", Toast.LENGTH_LONG).show();
+
+                // Exit to main activity
+                startActivity(new Intent(KeeperActivity.this, MainActivity.class));
+            }
+        }); // saveButton onClick
 
     } // onCreate()
 
@@ -46,6 +76,7 @@ public class KeeperActivity extends AppCompatActivity {
             petImageView.setImageDrawable(getResources().getDrawable(R.drawable.cat));
         }
     } // setUp()
+
 
 } // KeeperActivity
 
